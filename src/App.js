3f42f682d594ed9a8f058a265d2d8d93bd2cf76d
Shadman <shadmanchowdhury01@gmail.com>
2022-11-28 22:47:01 -0500

    feat: configured routes to frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import LandingPage from './pages/LandingPage';
+import {Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div>
-      <LandingPage/>
-    </div>
+     <div className="App">
+      <Routes>
+        <Route path="/" element={<LandingPage />}/>
+      </Routes>
+     </div>
+
   );
 }
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import Header from '../components/Header';
 import CardText from '../components/CardText';
+import {Link} from "react-router-dom"
 
 const useStyles = makeStyles((theme) => ({
     root: {
