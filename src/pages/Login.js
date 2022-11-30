10a7a08ddc65c73d85d35864322d3dbd65559eb7
Shadman <shadmanchowdhury01@gmail.com>
2022-11-29 20:22:05 -0500

    style: fixed issue with mismatching colors

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,23 +2,44 @@ import React, { Component } from "react";
 import "../styles.css";
 import CustomInput from "../components/CustomInput";
 import Button from "../components/Button";
-import bg from "./bg.jpg"; 
+import { AppBar, Toolbar } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import {Link} from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  appbar: {
+    background: '#3C4E5C!important',
+  },
+  appbarWrapper: {
+    width: '80%',
+    margin: '0 auto',
+  },
+  appbarTitle: {
+    flexGrow: '1',
+  },
+}));
 
 export default class Login extends Component {
   state = {
     email: "",
     password: ""
   };
-
   handleChange = e => {
     this.setState({ [e.currentTarget.id]: e.currentTarget.value });
   };
   render() {
-    console.log(bg);
     return (
       <div className="Login">
+        <AppBar style={{ background: '#2E3B55' }}>
+        <Toolbar className={useStyles.appbarWrapper}>
+          <Link to="/"style= {{textDecoration: 'none', color: 'white'}}><h1 className={useStyles.appbarTitle}>
+            mentee.<span className={useStyles.colorText}></span>
+          </h1></Link>
+        </Toolbar>
+        </AppBar>
         <form className="form">
           <CustomInput
+            style={{color:'white !important'}}
             labelText="Email"
             id="email"
             formControlProps={{
@@ -28,6 +49,7 @@ export default class Login extends Component {
             type="text"
           />
           <CustomInput
+            style={{fontSize: '2rem'}}
             labelText="Password"
             id="password"
             formControlProps={{
@@ -37,7 +59,7 @@ export default class Login extends Component {
             type="password"
           />
 
-          <Button type="button" color="primary" className="form__custom-button">
+          <Button style={{"&:hover": {backgroundColor: "blue !important"}}}type="button" className="form__custom-button">
             Log in
           </Button>
         </form>
