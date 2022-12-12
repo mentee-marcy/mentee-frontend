a968f30b076e08b41979e3fd2d123986ba0412ad
josemazhuvanchery1 <jose1998mathew@gmail.com>
2022-12-12 17:57:40 -0500

    real time messages

--- src/pages/Message.js
+++ src/pages/Message.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Sidebar from '../components/SideBar';
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from 'axios';
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
@@ -8,6 +8,7 @@ import "./CSS/messageBox.css"
 import Contacts from '../components/Contacts';
 import Welcome from '../components/Welcome';
 import ChatContainer from '../components/ChatContainer';
+import {io} from "socket.io-client"
 
 //const socket = io.connect("http://localhost:8000");
 const Message = () => {
@@ -17,20 +18,46 @@ const Message = () => {
   // const [chat, setChat] = React.useState([]);
   // const scrollRef = React.useRef();
 
-
+  const socket = useRef();
+ 
 
   const [contacts, setContacts] = useState([]);
   const [currentChat, setCurrentChat] = useState(undefined);
-  const [user, setUser] = useState(1)
+  const [user, setUser] = useState(null)
+
+  const token = localStorage.getItem('token');
+  //console.log(token)
+  const config = {
+    headers:{
+      "x-access-token": token
+    }
+  };
+  async function getUser(){
+    const user = await axios.get(`http://localhost:8000/users/profile`,config).then(data=>data.data)
+    console.log(user)
+    setUser(user.id)
+  }
+  useEffect(()=>{
+    getUser();
+    if(user) getFriends();
+  },[user])
+
+  useEffect(()=>{
+    if(user){
+      function getCurrentUser(){
+        socket.current = io("http://localhost:8000")
+        socket.current.emit("add-user",user)
+      }
+      getCurrentUser()
+    }
+  },[user])
+
   async function getFriends(){
     const data = await axios.get(`http://localhost:8000/users/${user}/friends`)
     const friends = data.data;
     const filterFriends = friends.filter((frnd) => frnd.id !== user);
     setContacts(filterFriends)
   }
-  useEffect(()=>{
-    getFriends();
-  },[user])
 
   const handleChatChange = (chat) =>{
     setCurrentChat(chat)
@@ -42,7 +69,7 @@ const Message = () => {
           {currentChat === undefined ? (
             <Welcome />
           ) : (
-            <ChatContainer currentChat={currentChat} user={user}/>
+            <ChatContainer currentChat={currentChat} user={user} socket={socket}/>
           )}
         </div>
     </div> 
