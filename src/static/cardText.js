270f8346c09a8e6895797654e39df27bf114a074
Shadman <shadmanchowdhury01@gmail.com>
2022-12-12 20:34:14 -0500

    style: changed wording on landing page to avoid confusions

--- src/static/cardText.js
+++ src/static/cardText.js
@@ -1,6 +1,6 @@
 const cardText = [
   {
-    title: 'What is Mentee?',
+    title: 'Why become a Mentor?',
     description:
       "Mentee builds a diverse community where non-traditional learners receive mentorship from tech professionals through communication and collaboration to build a more diverse industry by shortening the margin of non-traditional backgrounds.",
     imageUrl: process.env.PUBLIC_URL + '/assets/mentor.jpg',
@@ -9,7 +9,7 @@ const cardText = [
     link: '/registerMentor',
   },
   {
-    title: 'Why become a Mentee?',
+    title: 'What is a Mentee?',
     description:
       ' Mentee is an application for high-school graduates or GED holders from underprivileged communities and nontraditional backgrounds looking to build career skills through mentorship and community. Mentees can practice technical code, learn what a career in tech looks like, and receive support from mentors and Mentee friends via live communication throughout their career journey. ',
     imageUrl: process.env.PUBLIC_URL + '/assets/mentee.jpg',
