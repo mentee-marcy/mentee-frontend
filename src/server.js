c92558818d3778955340bf7391d20b5267a97004
Shadman <shadmanchowdhury01@gmail.com>
2022-12-15 23:12:35 -0500

    netlify

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: ['http://localhost:3000', 'https://mentee.netlify.app'],
 		methods: [ "GET", "POST" ],
 		credentials: true
 	}
